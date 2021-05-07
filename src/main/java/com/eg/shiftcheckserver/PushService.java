febe70ac276b8e80a1391d37decf57982e0f7643
makewheels <makewheels>
2021-05-07 23:37:18 +0800

    access token

--- src/main/java/com/eg/shiftcheckserver/PushService.java
+++ src/main/java/com/eg/shiftcheckserver/PushService.java
@@ -1,9 +1,12 @@
 package com.eg.shiftcheckserver;
 
+import com.github.makewheels.HttpUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PushService {
@@ -13,6 +16,8 @@ public class PushService {
     public void pushToWechatMiniProgram(
             List<String> openIds, String banName, String week, String time) {
         String accessToken = accessTokenService.getAccessToken();
-
+        Map<String, String> map = new HashMap<>();
+        String url = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=" + accessToken;
+        HttpUtil.post(url, map);
     }
 }
