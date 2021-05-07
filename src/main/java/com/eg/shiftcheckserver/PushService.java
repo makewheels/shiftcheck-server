751dcd441eb7c6e403af0c73d966864b545a6d8b
makewheels <makewheels>
2021-05-07 23:37:35 +0800

    access token

--- src/main/java/com/eg/shiftcheckserver/PushService.java
+++ src/main/java/com/eg/shiftcheckserver/PushService.java
@@ -19,5 +19,29 @@ public class PushService {
         Map<String, String> map = new HashMap<>();
         String url = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=" + accessToken;
         HttpUtil.post(url, map);
+        /**
+         * {
+         *   "touser": "OPENID",
+         *   "template_id": "TEMPLATE_ID",
+         *   "page": "index",
+         *   "miniprogram_state":"developer",
+         *   "lang":"zh_CN",
+         *   "data": {
+         *       "number01": {
+         *           "value": "339208499"
+         *       },
+         *       "date01": {
+         *           "value": "2015年01月05日"
+         *       },
+         *       "site01": {
+         *           "value": "TIT创意园"
+         *       } ,
+         *       "site02": {
+         *           "value": "广州市新港中路397号"
+         *       }
+         *   }
+         * }
+         * 订阅消息参数
+         */
     }
 }
